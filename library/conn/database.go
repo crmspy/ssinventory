f3c7d57af3322eae8d35d4d71d71cd8dc883b6ab
Nurul Hidayat <crmspy@gmail.com>
2019-01-02 22:23:04 +0700

    support mysql database

--- library/conn/database.go
+++ library/conn/database.go
@@ -1,7 +1,9 @@
 package conn
 import (
 	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/sqlite"
+    _ "github.com/jinzhu/gorm/dialects/sqlite"
+    _ "github.com/jinzhu/gorm/dialects/mysql"
+    _ "github.com/jinzhu/gorm/dialects/postgres"
 	log "github.com/Sirupsen/logrus"
 	"github.com/spf13/viper"
 )
@@ -10,18 +12,36 @@ var (
 	Db *gorm.DB
 )
 
+
 func DbInit() (){
-	//open a db connection now is only support sqlite and mysql
+    /*
+    open a db connection now is only support sqlite and mysql
+    this configuration read from config/app.json file
+    */
 	var err error
-	dbdriver := viper.GetString("database")
+    dbdriver := viper.GetString("database")
 	if dbdriver=="sqlite"{
-		Db, err = gorm.Open("sqlite3", "db/inventory.db")
+		Db, err = gorm.Open("sqlite3", viper.GetString("sqlite.file"))
 	}else if dbdriver=="mysql"{
-		Db, err = gorm.Open("mysql", "root:@/ssinventory?charset=utf8&parseTime=True&loc=Local")
-	}
+        var host string = viper.GetString("mysql.host")
+        var port string = viper.GetString("mysql.port")
+        var username string = viper.GetString("mysql.username")
+        var password string = viper.GetString("mysql.password")
+        var database string = viper.GetString("mysql.database")
+
+		Db, err = gorm.Open("mysql", username+":"+password+"@tcp("+host+":"+port+")/"+database+"?charset=utf8&parseTime=True&loc=Local")
+	}else if dbdriver=="postgresql"{
+        /*
+            Work In Progress 
+            table success imported is m_inventories and m_product other that failed to migrate
+        */
+        Db, err = gorm.Open("postgres", "host=localhost port=5432 user=postgres dbname=ssinventory password=postgres sslmode=disable")
+        
+    }
 	log.Print("try to connect to ",dbdriver)
 	
 	if err != nil {
+        log.Println(err)
 		panic("failed to connect database")
 	}
 	log.Println("connected to database")
